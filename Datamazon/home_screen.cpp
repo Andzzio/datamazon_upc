d475b1633d3d7b2fa1afcb836abe9c59e80ba4ee
andzzio <andzzio924@gmail.com>
2026-04-20 21:22:00 -0500

    Initial Datamazon Commit

---
+++ Datamazon/home_screen.cpp
@@ -0,0 +1,7 @@
+#include "home_screen.h"
+
+using namespace Datamazon;
+
+void main() {
+	Application::Run(gcnew home_screen());
+}
---
+++ Datamazon/home_screen.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace Datamazon {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de home_screen
+	/// </summary>
+	public ref class home_screen : public System::Windows::Forms::Form
+	{
+	public:
+		home_screen(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~home_screen()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// home_screen
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(631, 511);
+			this->Name = L"home_screen";
+			this->Text = L"home_screen";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
